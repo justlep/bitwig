0c93502bfd6544f3adae631b28fc9cf0efa31cf5
lennart <lennart@neofonie.de>
2015-10-13 16:57:43 +0200

    test script for not-assignable usercontrols in Bitwig 1.2

---
+++ tmp/userControlsNotWorking/UserControlTest.control.js
@@ -0,0 +1,22 @@
+loadAPI(1);
+
+host.defineController('meme', 'UserControlTest', '1.0', '42577294-71a7-11e5-9d70-feff819cdc9f', 'github@justlep.net');
+host.defineMidiPorts(1, 0);
+
+function init() {
+    var CC = 25,
+        userControls = host.createUserControls(1),
+        control = userControls.getControl(0);
+
+    control.setLabel('invertedControl');
+
+    host.getMidiInPort(0).setMidiCallback(function(status, data1, data2) {
+        printMidi(status, data1, data2);
+        if (isChannelController(status) && data1 === CC) {
+			println('Updating value of invertedControl to ' + (127 - data2));
+			control.set(127 - data2, 128);
+        }
+    });
+}
+
+function exit() {}
