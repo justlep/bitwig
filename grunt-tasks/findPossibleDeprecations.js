361ec4d337aea87f9d880c4330b054a99af0b933
lennart <lennart@neofonie.de>
2015-11-17 17:49:48 +0100

    added task for finding deprecations (unfinished)

---
+++ grunt-tasks/findPossibleDeprecations.js
@@ -0,0 +1,24 @@
+/**
+ * Parses the API documentation deprecations list and searches for possible usages
+ * within the controller scripts.
+ */
+module.exports = function (grunt, opts) {
+    'use strict';
+
+    var fs = require('fs');
+
+    grunt.registerTask('findPossibleDeprecations', 'Finds usages of deprecated methods in the controller scripts.',
+        function() {
+            var jsSources = grunt.file.expand(['src/**/*.js', '!src/lib/**']);
+
+            // TODO scan deprecated.html from Bitwig folder for depreated methods;
+
+            grunt.log.writeln('TODO...');
+
+            jsSources.forEach(function(filename) {
+                var js = grunt.file.read(filename);
+
+                // TODO find usages
+            });
+        });
+};
