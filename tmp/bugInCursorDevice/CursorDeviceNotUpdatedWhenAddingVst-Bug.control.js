6ea47825654927d0b78aa76489d2e6f558fc504e
lennart <lennart@neofonie.de>
2015-11-17 14:33:56 +0100

    test script for cursordevice observers not firing when adding vst device

---
+++ tmp/bugInCursorDevice/CursorDeviceNotUpdatedWhenAddingVst-Bug.control.js
@@ -0,0 +1,37 @@
+loadAPI(1);
+
+// TODO check if fixed in Bitwig 1.3.1
+
+/**
+ * Bug in Bitwig 1.3 Controller API:
+ *
+ * How to reproduce:
+ *   1. Add one of Bitwig's own devices to a channel (Comb, Chorus, Compressor, ..)
+ *      -> observers of CursorDevice are triggered -> OK
+ *   2. Add a VST to a channel
+ *      -> NO observers of CursorDevice are triggered -> FAIL
+ *      -> switch to another device and then back to the VST -> observers are called -> OK
+ */
+
+host.defineController('meme', 'CursorDeviceNotUpdatedWhenAddingVst', '1.0', '6ac91cb4-8d22-11e5-8994-feff819cdc9f', 'github@justlep.net');
+host.defineMidiPorts(0, 0);
+
+function init() {
+    var cursorDevice = host.createCursorDevice();
+
+    cursorDevice.addNameObserver(40, 'unknown device', function(deviceName) {
+        println('Observer of CursorDevice.addNameObserver(..) reports: ' + deviceName);
+    });
+
+    cursorDevice.addPageNamesObserver(function() {
+        println('Observer of CursorDevice.addPageNamesObserver(..) reports:' + arguments);
+        // Other issue with addPageNamesObserver:
+        // https://github.com/justlep/bitwig/issues/2
+    });
+
+    cursorDevice.getCommonParameter(0).addValueObserver(128, function(val) {
+        println('Observer of CursorDevice.getCommonParameter(0).addValueObserver(..) reports: ' + val);
+    });
+}
+
+function exit() {}
