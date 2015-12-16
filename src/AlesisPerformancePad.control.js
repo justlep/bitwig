fcd8ccd18222a2cd915515dacd28b949e480947c
Lennart Pegel <github@justlep.net>
2015-12-16 19:55:53 +0100

    v1.0.7 adjustments in the PerformancePad script

--- src/AlesisPerformancePad.control.js
+++ src/AlesisPerformancePad.control.js
@@ -7,8 +7,6 @@
  *    triggering devices in Bitwig)
  * - Velocity curves can be chosen via preferences (normal, fixed values, lift+20, Gamma2, Gamma3, Gamma4)
  *
- * TODO: map HiHat+Kick pedals to notes
- *
  * Author: Lennart Pegel - https://github.com/justlep/bitwig
  * License: LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0.txt)
  */
@@ -16,7 +14,7 @@
 loadAPI(1);
 
 // @deprecationChecked:1.3.5
-host.defineController('Alesis', 'PerformancePad', '1.0', 'c6cc8a34-a36e-11e5-bf7f-feff819cdc9f', 'Lennart Pegel <github@justlep.net>');
+host.defineController('Alesis', 'PerformancePad', '1.1', 'c6cc8a34-a36e-11e5-bf7f-feff819cdc9f', 'Lennart Pegel <github@justlep.net>');
 host.defineMidiPorts(1, 0);
 
 var NOTE_MAPPING = {
@@ -28,17 +26,42 @@ var NOTE_MAPPING = {
         0x2D: 0x29,
         0x31: 0x2A,
         0x33: 0x2B // top right pad => mapped to G1
-    };
+    },
+    GATE_OPTIONS = ['5', '20', '30', '50', '70', '100'],
+    DEFAULT_GATE_OPTION = '20',
+    VELOCITY_MODES = ['ADD-20', 'ADD-30', 'FIX-30', 'FIX-60', 'FIX-90', 'FIX-110', 'FIX-127', 'GAMMA-2', 'GAMMA-3', 'GAMMA-4', 'LINEAR'],
+    DEFAULT_VELOCITY_MODE = 'GAMMA-2';
+
 
 function init() {
     var inPort = host.getMidiInPort(0),
-        noteInput = inPort.createNoteInput('PerformancePad', '000000'), // block everything and let midiCallback do the talking
+        noteInput = inPort.createNoteInput('Pads', '000000'), // block everything and let midiCallback do the talking
         gateTimeInMillis = 20,
         velocityTranslationTable = [],
         preferences = host.getPreferences(),
-        gateSetting = preferences.getEnumSetting('Gate (ms)', 'Preferences', ['5','20','30','50','70','100'], '20'),
-        velocitySetting = preferences.getEnumSetting('Velocity', 'Preferences',
-                                                     ['DEFAULT','32','64','96','127','LIFT','GAMMA2', 'GAMMA3', 'GAMMA4'], 'GAMMA2');
+        gateSetting = preferences.getEnumSetting('Gate (ms)', 'Preferences', GATE_OPTIONS, DEFAULT_GATE_OPTION),
+        velocitySetting = preferences.getEnumSetting('Velocity Curve', 'Preferences', VELOCITY_MODES, DEFAULT_VELOCITY_MODE),
+        notesRemapSetting = preferences.getEnumSetting('Remap pads', 'Preferences', ['C1 to G1','NO'], 'C1 to G1'),
+        isNoteRemapEnabled = true,
+        HANDLERS = {
+            MIDI: function(status, data1, data2) {
+                // printMidi(status, data1, data2);
+                if (status !== 0x90 || !data2) return;  // ignore anything that is not note-on
+
+                var targetNote = isNoteRemapEnabled ? NOTE_MAPPING[data1] : data1,
+                    targetVelocity;
+
+                if (targetNote) {
+                    targetVelocity = velocityTranslationTable[data2];
+                    // println('Note (orig/target): ' + data1 + '/' + targetNote + ', Vel (orig/target): ' + data2 + '/' + targetVelocity);
+                    noteInput.sendRawMidiEvent(0x90, targetNote, targetVelocity);
+                    host.scheduleTask(HANDLERS.TIMED_NOTE_OFF, [targetNote], gateTimeInMillis);
+                }
+            },
+            TIMED_NOTE_OFF: function(note) {
+                noteInput.sendRawMidiEvent(0x80, note, 0);
+            }
+        };
 
     gateSetting.addValueObserver(function(newGate) {
         gateTimeInMillis = parseInt(newGate, 10);
@@ -46,9 +69,9 @@ function init() {
     });
 
     velocitySetting.addValueObserver(function(velocityMode) {
-        var fixVelocity = parseInt(velocityMode, 10) || false,
-            liftOffset = (velocityMode === 'LIFT') ? 20 : 0,
-            gamma = (/GAMMA(\d)/.test(velocityMode)) ? parseInt(RegExp.$1, 10) : 0;
+        var fixVelocity = /FIX-(\d+)/.test(velocityMode) ? parseInt(RegExp.$1, 10) : false,
+            liftOffset = /ADD-(\d+)/.test(velocityMode) ? parseInt(RegExp.$1, 10) : 0,
+            gamma = (/GAMMA-(\d)/.test(velocityMode)) ? parseInt(RegExp.$1, 10) : 0;
 
         for (var v = 0; v < 128; v++) {
             velocityTranslationTable[v] = gamma ? Math.round(127 * Math.pow(v/127, 1/gamma)) : (fixVelocity || Math.min(v + liftOffset, 127));
@@ -57,22 +80,13 @@ function init() {
         // println('Changed velocity mode: ' + velocityMode);
     });
 
-    noteInput.setShouldConsumeEvents(false);
-
-    inPort.setMidiCallback(function(status, data1, data2) {
-        // printMidi(status, data1, data2);
-        var targetNote = (status === 0x90) && data2 && NOTE_MAPPING[data1] || 0,
-            targetVelocity;
-
-        if (targetNote) {
-            targetVelocity = velocityTranslationTable[data2];
-            // println('Note (orig/target): ' + data1 + '/' + targetNote + ', Vel (orig/target): ' + data2 + '/' + targetVelocity);
-            noteInput.sendRawMidiEvent(0x90, targetNote, targetVelocity);
-            host.scheduleTask(function(note) {
-                noteInput.sendRawMidiEvent(0x80, note, 0);
-            }, [targetNote], gateTimeInMillis);
-        }
+    notesRemapSetting.addValueObserver(function(remap) {
+        isNoteRemapEnabled = (remap !== 'NO');
+        println('Note remap enabled: ' + isNoteRemapEnabled);
     });
+
+    noteInput.setShouldConsumeEvents(false);
+    inPort.setMidiCallback(HANDLERS.MIDI);
 
     println('\n--------------\nPerformancePad ready');
 }
