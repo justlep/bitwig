75bca9e1a5249b1ad53f9f643d9813730646fd06
Lennart Pegel <github@justlep.net>
2016-05-08 09:56:06 +0200

    added GreedyParamsValueSet for BCR2000

---
+++ src/lep/value/GreedyParamsValueSet.js
@@ -0,0 +1,37 @@
+/**
+ * @constructor
+ */
+lep.GreedyParamsValueSet = lep.util.extendClass(lep.ValueSet, {
+
+    _init: function(primaryCursorDevice, paramPages) {
+        lep.util.assertObject(primaryCursorDevice, 'Invalid primaryCursorDevice for ParamsValueSet');
+        lep.util.assertNumberInRange(paramPages, 1, 12);
+
+        var FIX_PARAM_PAGES_COUNT = 2,
+            _cursorDevices = [primaryCursorDevice];
+
+        this._super('ParamsValueSet', paramPages, 8, function(groupIndex, paramIndex) {
+            switch(groupIndex) {
+                case 0: return lep.StandardRangedValue.createCommonParamValue(primaryCursorDevice, paramIndex);
+                case 1: return lep.StandardRangedValue.createEnvelopeParamValue(primaryCursorDevice, paramIndex);
+                case 2: return lep.StandardRangedValue.createParamValue(primaryCursorDevice, paramIndex);
+                default:
+                    var additionalCursorIndex = (groupIndex - FIX_PARAM_PAGES_COUNT),
+                        additionalCursorDevice = paramIndex ? _cursorDevices[additionalCursorIndex] : host.createEditorCursorDevice(),
+                        paramPageIndex = (groupIndex - FIX_PARAM_PAGES_COUNT);
+
+                    if (!paramIndex) {
+                        _cursorDevices[additionalCursorIndex] = additionalCursorDevice;
+                        additionalCursorDevice.addPageNamesObserver(function(/* unused names as arguments */) {
+                            lep.logDev('additionalCursorDevice#{}.setParameterPage({})', additionalCursorIndex, paramPageIndex);
+                            additionalCursorDevice.setParameterPage(paramPageIndex);
+                        });
+                    }
+                    return lep.StandardRangedValue.createParamValue(additionalCursorDevice, paramIndex);
+            }
+        });
+    }
+
+    // TODO add page recall for previously selected devices
+
+});
