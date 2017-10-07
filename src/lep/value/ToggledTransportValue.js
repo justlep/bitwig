9babe720475acb594223cbad523c3ca0436d2e35
Lennart Pegel <github@justlep.net>
2017-10-07 21:35:53 +0200

    added calculateVelocity option

--- src/lep/value/ToggledTransportValue.js
+++ src/lep/value/ToggledTransportValue.js
@@ -12,23 +12,27 @@ lep.ToggledTransportValue = lep.util.extendClass(lep.BaseValue, {
         this._super(opts);
 
         lep.util.assert(!lep.ToggledTransportValue._instances[this.name], 'Must use lep.ToggledTransportValue.getXXXXInstance');
+        lep.util.assertFunctionOrEmpty(opts.calculateVelocity, 'Invalid opts.calculateVelocity for {}', this.name);
         lep.ToggledTransportValue._instances[this.name] = this;
 
         var self = this,
             transport = lep.util.getTransport(),
-            settableBoolean = transport[opts.booleanPropertyName]();
+            settableBoolean = transport[opts.booleanPropertyName](),
+            calcVelocity = opts.calculateVelocity || null;
 
         lep.util.assert(settableBoolean, 'Invalid settableBoolean {} in Transport for {}',
             opts.booleanPropertyName, this.name);
 
         this.toggleOnPressed = (opts.toggleOnPressed !== false);
+        this.isOn = null;
 
         this.togglingMethod = function() {
             settableBoolean.toggle();
         };
 
         settableBoolean.addValueObserver(function(isOn) {
-            self.value = isOn ? 127 : 0;
+            self.isOn = isOn;
+            self.value = calcVelocity ? calcVelocity(isOn) : isOn ? 127 : 0;
             self.syncToController();
         });
     },
@@ -60,10 +64,11 @@ lep.ToggledTransportValue._instances = {};
     lep.util.assertString(instanceName && transportBooleanPropertyName,
         'Bad call of _makeInstanceGetter for ToggledTransportValue, instanceName = {}', instanceName);
 
-    return function() {
+    return function(optCalcVelocityFn) {
         return lep.ToggledTransportValue._instances[instanceName] || new lep.ToggledTransportValue({
             name: instanceName,
-            booleanPropertyName: transportBooleanPropertyName
+            booleanPropertyName: transportBooleanPropertyName,
+            calculateVelocity: optCalcVelocityFn
         });
     };
 });
