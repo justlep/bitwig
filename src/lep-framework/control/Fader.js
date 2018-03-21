ea066e780cbfaa1369957a37e9a3a00d1326cdc8
Lennart Pegel <github@justlep.net>
2018-03-22 00:15:53 +0100

    added muteOnTouch option

--- src/lep-framework/control/Fader.js
+++ src/lep-framework/control/Fader.js
@@ -8,5 +8,30 @@
 lep.Fader = lep.util.extendClass(lep.BaseControl, {
     _init: function(opts) {
         this._super(opts);
+
+        if (opts.muteOnTouch) {
+            this.initMuteOnTouch(opts.touchNote, opts.touchCC, (typeof opts.touchChannel === 'number') ? opts.touchChannel : opts.midiChannel);
+        }
+    },
+    initMuteOnTouch: function(touchNote, touchCC, touchChannel) {
+        var useTouchCC = typeof touchCC === 'number',
+            useTouchNote = (typeof touchNote === 'number');
+
+        lep.util.assertNumberInRange(touchChannel, 0, 15, 'Invalid touchChannel for {}: {}', this.name, touchChannel);
+        lep.util.assert(useTouchCC !== useTouchNote, 'Invalid combination of touchNote {} / touchCC {} for {}',
+            touchNote, touchCC, this.name);
+
+        if (useTouchNote) {
+            lep.util.assertNumberInRange(touchNote, 0, 127, 'Invalid touchNote for {}: {}', this.name, touchNote);
+            lep.MidiEventDispatcher.getInstance().onNotePressed(touchNote, this._muteOrUnmuteTouchHandler, this, touchChannel);
+            lep.MidiEventDispatcher.getInstance().onNoteReleased(touchNote, this._muteOrUnmuteTouchHandler, this, touchChannel);
+        } else {
+            lep.util.assertNumberInRange(touchCC, 0, 127, 'Invalid touchCC for {}: {}', this.name, touchCC);
+            lep.MidiEventDispatcher.getInstance().onCC(touchCC, this._muteOrUnmuteTouchHandler, this, touchChannel);
+        }
+    },
+    _muteOrUnmuteTouchHandler: function(noteOrCc, value, channel) {
+        this.syncToMidi = value ? lep.util.NOP : lep.BaseControl.prototype.syncToMidi;
+        // lep.logDev('{} is now {}', this.name, value ? 'MUTED' : 'UN-MUTED');
     }
 });
